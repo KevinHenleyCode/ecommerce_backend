42b87fd3b2806958769c17840a2e715ae56d206c
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-06-01 03:21:48 -0500

    added columns to each of the models

--- models/Product.js
+++ models/Product.js
@@ -8,8 +8,30 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+        validate:{
+          isDecimal: true
+        }
+    },
+    catagory_id: {
+      type: DataTypes.INTEGER,
+      references: 'catagory',
+      referencesKey: 'id'
+    }
   },
   {
     sequelize,
